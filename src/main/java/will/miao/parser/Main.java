cc0f6c54dd7b5bcd12dd1180e333569a5586270d
Will Miao <miaoyongqiang@huawei.com>
2016-09-10 07:00:49 +0800

    Initial commit

---
+++ src/main/java/will/miao/parser/Main.java
@@ -0,0 +1,11 @@
+package will.miao.parser;
+
+/**
+ * Created by WillMiao on 16/9/10.
+ */
+public class Main {
+	public static void main(String[] args) {
+		// TODO:
+		QueryParser parser = new QueryParser();
+	}
+}
