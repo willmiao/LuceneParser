e72907d5272080f7cfbff1da852c995318ea9d88
Will Miao <miaoyongqiang@huawei.com>
2016-09-11 13:29:15 +0800

    Fix

--- src/main/java/will/miao/parser/Main.java
+++ src/main/java/will/miao/parser/Main.java
@@ -1,11 +1,17 @@
 package will.miao.parser;
 
-import org.apache.lucene.search.Query;
-
 /**
  * Created by WillMiao on 16/9/10.
  */
 public class Main {
+	private static final String[] PREDICATES = {
+			"host IN ('host1', 'host3', 'host4')",
+			"host = 'host2' OR time >= 400 && price <= 3.14",
+			"host = 'host2' AND time >= 400 || price <= 3.14",
+			"(host = 'host2' AND time >= 400) || price <= 3.14",
+			"(host = 'host2' OR time >= 400) && price <= 3.14",
+			"host = 'host2' OR (time >= 400 && price <= 3.14)"
+	};
 	public static void main(String[] args) throws ParseException {
 		// TODO:
 		String[] fieldNames = {"host", "content", "time", "price"};
@@ -13,8 +19,10 @@ public class Main {
 
 		QueryParser queryParser = new QueryParser(fieldNames, fieldTypes);
 
-		Query query = queryParser.parse("price BETWEEN 3.14 AND 10.8 OR time >= 400");
-
-		System.out.println(query);
+		for (String predicate : PREDICATES) {
+			System.out.println(predicate);
+			System.out.println(queryParser.parse(predicate));
+			System.out.println("*************************************************************");
+		}
 	}
 }
